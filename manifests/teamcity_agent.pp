0f09cbc66d20206c0baa539ce6a0e859e5eeb0ce
Dan Gibbons <dan.gibbons@rbi.co.uk>
2017-02-02 13:44:59 +0000

    Adding .net 4.5.2 packages

--- manifests/teamcity_agent.pp
+++ manifests/teamcity_agent.pp
@@ -1,5 +1,6 @@
 class role::teamcity_agent () {
   include ::profile::teamcity::agent
+  include ::profile::windows::software::dotnet452
   include ::profile::windows::software::chrome
   include ::profile::windows::software::notepadplusplus
   include ::profile::windows::software::windows71sdk
@@ -11,4 +12,5 @@ class role::teamcity_agent () {
   include ::profile::windows::software::rubydevkit
   include ::profile::windows::software::bundler
   include ::profile::windows::software::buildtools2015
+  include ::profile::windows::software::dotnet451developerpack
 }
